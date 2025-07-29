d3effde580b7a738d4891e53f19dd61b8b35d3df
Kevin MacMartin <prurigro@gmail.com>
2025-07-28 22:27:39 -0400

    Populate a list with the available desktop files

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"git.darkcloud.ca/kevin/gnome-appcat-manager/xdg"
+	"git.darkcloud.ca/kevin/gnome-appcat-manager/ui"
 )
 
 func main() {
-	// Print out the list of desktop files
-	for _, file := range xdg.DisplayFiles {
-		fmt.Println(file)
-	}
+	ui.CategoriesList();
 }
---
+++ ui/ui.go
@@ -0,0 +1,137 @@
+package ui
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"git.darkcloud.ca/kevin/gnome-appcat-manager/xdg"
+)
+
+// Style
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(1)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(3)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("12"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(3)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(3).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 2)
+)
+
+// List item definition
+type item string
+
+func (i item) FilterValue() string {
+	return ""
+}
+
+var items = []list.Item{}
+
+// List item format
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int { return 1 }
+func (d itemDelegate) Spacing() int { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+	fn := itemStyle.Render
+
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+// Data model
+type model struct {
+	list     list.Model
+	choice   string
+	quitting bool
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+		case tea.WindowSizeMsg:
+			m.list.SetSize(msg.Width, msg.Height - 1)
+			return m, nil
+
+		case tea.KeyMsg:
+			switch keypress := msg.String(); keypress {
+
+			case "q", "ctrl+c":
+				m.quitting = true
+				return m, tea.Quit
+
+			case "enter":
+				i, ok := m.list.SelectedItem().(item)
+
+				if ok {
+					m.choice = string(i)
+				}
+
+				return m, tea.Quit
+			}
+	}
+
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	if m.choice != "" {
+		return quitTextStyle.Render(fmt.Sprintf("Selecting %s", m.choice))
+	}
+
+	if m.quitting {
+		return quitTextStyle.Render("Quitting...")
+	}
+
+	return "\n" + m.list.View()
+}
+
+func LoadList(title string) {
+	l := list.New(items, itemDelegate{}, 1, 1)
+	l.Title = title
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
+	m := model{list: l}
+
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
+
+func CategoriesList() {
+	items = nil
+
+	for _, file := range xdg.DisplayFiles {
+		items = append(items, item(file))
+	}
+
+	LoadList("Select an application")
+}
