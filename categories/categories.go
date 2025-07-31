1a12d9ade062741d9bc947c51272f41a852c1ae9
Kevin MacMartin <prurigro@gmail.com>
2025-07-30 23:18:01 -0400

    Build out initial category list scanning

---
+++ categories/categories.go
@@ -0,0 +1,98 @@
+package categories
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	// "slices"
+	// "sort"
+	// "strings"
+)
+
+type category struct {
+	file string
+	name string
+	applications []string
+}
+
+var (
+	Categories = []category{}
+	categoriesDirectory = os.Getenv("XDG_DATA_HOME") + "/gnome-shell/categories"
+)
+
+func createCatDirWhenMissing() {
+	if stat, err := os.Stat(categoriesDirectory); err != nil || !stat.IsDir() {
+		os.MkdirAll(categoriesDirectory, 0755)
+	}
+}
+
+func populateCategories() {
+	var files []string
+
+	createCatDirWhenMissing()
+	categoryFiles, _ := os.ReadDir(categoriesDirectory)
+
+	// Regex to pull just the name out of the category file
+	catNameRegex := regexp.MustCompile("(?i)^ *(.*)\\.category *$")
+
+	// Regex to ensure we're only looking at .desktop files
+	xdgMatchRegex := regexp.MustCompile("(?i)^ *(.*)\\.desktop *$")
+
+	for _, catFile := range categoryFiles {
+		if catNameRegex.MatchString(catFile.Name()) {
+			files = nil
+			catNameMatchReference := catNameRegex.FindStringSubmatch(catFile.Name())
+			filePath := categoriesDirectory + "/" + catFile.Name()
+			file, err := os.Open(filePath)
+
+			if err != nil {
+				log.Fatal("Unable to open the file " + filePath)
+			}
+
+			scanner := bufio.NewScanner(file)
+
+			for scanner.Scan() {
+				if xdgMatchRegex.MatchString(scanner.Text()) {
+					files = append(files, xdgMatchRegex.FindStringSubmatch(scanner.Text())[0])
+				}
+			}
+
+			Categories = append(Categories, category{ file: catFile.Name(), name: catNameMatchReference[1], applications: []string{} })
+
+			// // Sort alphabetically (case insensitive)
+			// sort.Slice(list, func(x, y int) bool {
+			// 	return strings.ToLower(list[x]) < strings.ToLower(list[y])
+			// })
+		}
+	}
+
+	fmt.Println(Categories)
+}
+
+// Returns an array of file names
+func FileNames() []string {
+	var fileNames []string
+
+	for _, item := range Categories {
+		fileNames = append(fileNames, item.file)
+	}
+
+	return fileNames
+}
+
+// Returns an array of app names
+func CatNames() []string {
+	var catNames []string
+
+	for _, item := range Categories {
+		catNames = append(catNames, item.name)
+	}
+
+	return catNames
+}
+
+func init() {
+	populateCategories()
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"git.darkcloud.ca/kevin/gnome-appcat-manager/categories"
 	"git.darkcloud.ca/kevin/gnome-appcat-manager/xdg"
 	"git.darkcloud.ca/kevin/gnome-appcat-manager/ui"
 )
@@ -9,13 +10,19 @@ import (
 var uiResponse int
 
 func main() {
-	uiResponse = ui.LoadList("Main Menu", []string{"List Applications", "Quit"});
+	uiResponse = ui.LoadList("Main Menu", []string{"List Applications", "List Categories", "Quit"});
 
 	if (uiResponse == 0) {
 		uiResponse = ui.LoadList("Select an application", xdg.AppNames());
 
 		if (uiResponse != -1) {
 			fmt.Println(xdg.FileNames()[uiResponse])
+		}
+	} else if (uiResponse == 1) {
+		uiResponse = ui.LoadList("Select a category", categories.CatNames());
+
+		if (uiResponse != -1) {
+			fmt.Println(categories.FileNames()[uiResponse])
 		}
 	} else {
 		fmt.Println("Quitting...")
