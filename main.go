1963d9be9becafd0c67f8b8ba0c51e4a0cff00e3
Kevin MacMartin <prurigro@gmail.com>
2025-08-03 01:19:28 -0400

    Update the manageCategories function so it lets you choose the category for an application, track the application index when moving back, include the app name in the title text when selecting the category, and start by selecting the same category as the application is currently in

--- main.go
+++ main.go
@@ -9,18 +9,20 @@ import (
 
 var (
 	uiResponse int // -1 is quit, -2 is back
-	mainMenu = []string{"Categories", "Quit"}
+	mainMenu = []string{"Categorize Applications", "Quit"}
 )
 
 func manageCategories() {
 	var (
-		prevCategoryIndex int = 0
-		prevApplicationIndex int = 0
+		catIndex int = 0
+		appIndex int = 0
+		newCatIndex int = 0
+		appLauncher xdg.Launcher
 	)
 
 	for {
-		uiResponse = ui.LoadList("Select a category", categories.CatNames, prevCategoryIndex);
-		prevCategoryIndex = uiResponse
+		uiResponse = ui.LoadList("Select a category to edit", categories.CatNames, catIndex);
+		catIndex = uiResponse
 
 		if uiResponse == -1 || uiResponse == -2 {
 			return
@@ -28,16 +30,26 @@ func manageCategories() {
 
 		catIndex := uiResponse
 		category := categories.List[catIndex]
-		prevApplicationIndex = 0
+		appIndex = 0
 
 		for {
-			uiResponse = ui.LoadList("Select an application", xdg.GetAppNames(category.Applications), prevApplicationIndex)
-			prevApplicationIndex = uiResponse
+			uiResponse = ui.LoadList("Select an application to categorize", xdg.GetAppNames(category.Applications), appIndex)
 
 			if uiResponse == -1 {
 				return
 			} else if uiResponse == -2 {
 				break
+			} else {
+				appIndex = uiResponse
+				appLauncher = category.Applications[appIndex]
+				uiResponse = ui.LoadList("Select a new category for " + appLauncher.AppName, categories.CatNames, catIndex);
+
+				if uiResponse == -1 {
+					return
+				} else if uiResponse != -2 {
+					newCatIndex = uiResponse
+					fmt.Println(newCatIndex)
+				}
 			}
 		}
 	}
