c5ef4059f0cafdc265e66b885c07e34b90bb1222
Kevin MacMartin <prurigro@gmail.com>
2025-08-06 00:08:05 -0400

    Add the ability to apply and clear categories in gnome

---
+++ gnome/gnome.go
@@ -0,0 +1,90 @@
+package gnome
+
+import (
+	"encoding/json"
+	"log"
+	"os/exec"
+	"git.darkcloud.ca/kevin/gnome-appcat-manager/application"
+	"git.darkcloud.ca/kevin/gnome-appcat-manager/category"
+)
+
+func ClearCategories() () {
+	var (
+		err error
+		cmd *exec.Cmd
+	)
+
+	// Reset the category list
+	cmd = exec.Command("gsettings", "set", "org.gnome.desktop.app-folders", "folder-children", "[]")
+	_, err = cmd.Output()
+
+	if err != nil {
+		log.Fatal("Error resetting the category list in dconf:", err)
+	}
+
+	// Reset the category folders
+	cmd = exec.Command("dconf", "reset", "-f", "/org/gnome/desktop/app-folders/folders/")
+	_, err = cmd.Output()
+
+	if err != nil {
+		log.Fatal("Error resetting the category folders in dconf:", err)
+	}
+}
+
+func ApplyCategories() {
+	var (
+		categories []category.Data = category.GetListWithoutUncategorized()
+		cmd *exec.Cmd
+		err error
+		jsonList []byte
+	)
+
+	for _, catItem := range categories {
+		// Create the category folder
+		cmd = exec.Command("gsettings", "set", "org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/" + catItem.Name + "/", "name", catItem.Name)
+		_, err = cmd.Output()
+
+		if err != nil {
+			log.Fatal("Error creating the category folder in dconf:", err)
+		}
+
+		// Add applications to the category if any exist
+		if len(catItem.Applications) > 0 {
+			jsonList, err = json.Marshal(application.GetFiles(catItem.Applications))
+
+			if err != nil {
+				log.Fatal("Error converting " + catItem.Name + " application names to json:", err)
+			}
+
+			cmd = exec.Command("gsettings", "set", "org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/" + catItem.Name + "/", "apps", string(jsonList))
+			_, err = cmd.Output()
+
+			if err != nil {
+				log.Fatal("Error adding " + catItem.Name + " applications in dconf:", err)
+			}
+		}
+	}
+
+	// Build the category list
+	jsonList, err = json.Marshal(category.GetNames(categories))
+
+	if err != nil {
+		log.Fatal("Error converting category names to json:", err)
+	}
+
+	// Apply the category list
+	cmd = exec.Command("gsettings", "set", "org.gnome.desktop.app-folders", "folder-children", string(jsonList))
+	_, err = cmd.Output()
+
+	if err != nil {
+		log.Fatal("Error applying the category list in dconf:", err)
+	}
+
+	// Reset the app picker layout
+	cmd = exec.Command("gsettings", "set", "org.gnome.shell", "app-picker-layout", "[]")
+	_, err = cmd.Output()
+
+	if err != nil {
+		log.Fatal("Error resetting the app picker layout in dconf:", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"encoding/json"
+	"time"
 	"git.darkcloud.ca/kevin/gnome-appcat-manager/application"
 	"git.darkcloud.ca/kevin/gnome-appcat-manager/category"
 	"git.darkcloud.ca/kevin/gnome-appcat-manager/color"
+	"git.darkcloud.ca/kevin/gnome-appcat-manager/gnome"
 	"git.darkcloud.ca/kevin/gnome-appcat-manager/ui"
 )
 
 var (
 	uiResponse int // -1 is quit, -2 is back
-	mainMenu = []string{"Manage Categories", "Create Category", "Delete Category", "Quit"}
+	mainMenu = []string{"Manage Application Categories", "Create New Category Folder", "Delete Existing Category Folder", "Apply Gnome Category Folders", "Clear Gnome Category Folders", "Quit"}
 	okCancelOptions = []string{"Confirm", "Cancel"}
 )
 
-// Interactive application categorization
-func manageCategories() {
+// Application category management
+func manageApplicationCategories() {
 	var (
 		catIndex int = 0
 		newCatIndex int = 0
@@ -22,7 +26,7 @@ func manageCategories() {
 	)
 
 	for {
-		uiResponse = ui.List("Select a " + color.Add("red", "category") + " to edit its " + color.Add("yellow", "applications"), category.GetNames(category.List), catIndex);
+		uiResponse = ui.List("Select a " + color.Add("red", "category folder") + " to edit its " + color.Add("yellow", "applications"), category.GetNames(category.List), catIndex);
 
 		if uiResponse == -1 || uiResponse == -2 {
 			return
@@ -32,7 +36,7 @@ func manageCategories() {
 		appIndex = 0
 
 		for {
-			uiResponse = ui.List("Select an " + color.Add("red", "application") + " to change its " + color.Add("yellow", "category"), application.GetNames(category.List[catIndex].Applications), appIndex)
+			uiResponse = ui.List("Select an " + color.Add("red", "application") + " to change its " + color.Add("yellow", "category folder"), application.GetNames(category.List[catIndex].Applications), appIndex)
 
 			if uiResponse == -1 {
 				return
@@ -41,7 +45,7 @@ func manageCategories() {
 			}
 
 			appIndex = uiResponse
-			uiResponse = ui.List("Select a new " + color.Add("red", "category") + " for " + color.Add("yellow", category.List[catIndex].Applications[appIndex].Name), category.GetNames(category.List), newCatIndex);
+			uiResponse = ui.List("Select a new " + color.Add("red", "category folder") + " for " + color.Add("yellow", category.List[catIndex].Applications[appIndex].Name), category.GetNames(category.List), newCatIndex);
 
 			if uiResponse == -1 {
 				return
@@ -53,33 +57,35 @@ func manageCategories() {
 	}
 }
 
-func createCategory() {
+// Create a new category
+func createCategoryFolder() {
 	newCategory := ui.Input("Enter a new category name")
 
 	if newCategory != "" {
 		status, err := category.Create(newCategory)
 
 		if status {
-			ui.MessageWait("Successfully created " + color.Add("yellow", newCategory))
+			ui.MessageWait("Successfully created the " + color.Add("yellow", newCategory) + " category folder")
 		} else {
 			ui.MessageWait(err)
 		}
 	}
 }
 
-func deleteCategory() {
+// Delete an existing category
+func deleteCategoryFolder() {
 	var catIndex int = 0
 
 	for {
 		catNames := category.GetNames(category.GetListWithoutUncategorized())
-		uiResponse = ui.List("Select a " + color.Add("red", "category") + " to " + color.Add("yellow", "delete"), catNames, catIndex);
+		uiResponse = ui.List("Select a " + color.Add("red", "category folder") + " to " + color.Add("yellow", "delete"), catNames, catIndex);
 
 		if uiResponse == -1 || uiResponse == -2 {
 			return
 		}
 
 		catIndex = uiResponse
-		uiResponse = ui.List(color.Add("red", "Delete") + " the category " + color.Add("yellow", catNames[catIndex]) + "?", okCancelOptions, 0)
+		uiResponse = ui.List(color.Add("red", "Delete") + " the category folder " + color.Add("yellow", catNames[catIndex]) + "?", okCancelOptions, 0)
 
 		if uiResponse == -1 {
 			return
@@ -93,6 +99,30 @@ func deleteCategory() {
 	}
 }
 
+// Apply configured categories to the gnome application list
+func applyGnomeCategoryFolders() {
+	ui.Message("Clearing old category folders...")
+	gnome.ClearCategories()
+	time.Sleep(3 * time.Second)
+	ui.Message("Applying categories...")
+	gnome.ApplyCategories()
+	ui.MessageWait("Successfully applied gnome category folders")
+}
+
+// Clear the gnome application list categories
+func clearGnomeCategoryFolders() {
+	gnome.ClearCategories()
+	ui.MessageWait("Successfully cleared category folders")
+}
+
+
+type User struct {
+    Name string
+    Age int
+    Active bool
+    lastLoginAt string
+}
+// Main menu
 func main() {
 	var menuIndex = 0
 
@@ -102,15 +132,25 @@ func main() {
 
 		switch uiResponse {
 			case 0:
-				manageCategories()
+				manageApplicationCategories()
 
 			case 1:
-				createCategory()
+				createCategoryFolder()
 
 			case 2:
-				deleteCategory()
+				deleteCategoryFolder()
+
+			case 3:
+				applyGnomeCategoryFolders()
+
+			case 4:
+				clearGnomeCategoryFolders()
 		}
 	}
 
 	ui.Message("Quitting...")
+
+	// u, _ := json.Marshal(application.GetFiles(category.List[2].Applications))
+	u, _ := json.Marshal(category.GetNames(category.GetListWithoutUncategorized()))
+	fmt.Println(string(u))
 }
